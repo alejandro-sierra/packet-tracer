e7258cbf2ffeaeafe72738a1459e37c09eeac2a1
alejandro-sierra <alejandrosierraballesta@gmail.com>
2021-05-20 18:15:46 +0200

    11.01 finished

--- ud11/11.01/ejercicio11.sh
+++ ud11/11.01/ejercicio11.sh
@@ -35,15 +35,69 @@ else
 fi
 }
 
-#Petición de datos.
-#Se utiliza el while para comprobar que se ha introducido realmente un número
-error=0
-while [ $error -eq 0 ]; do
+opciones(){
+case $1 in
+    0)
+    #nada
+    printf "\t \tNo \t \tNo \t \tNo\n"
+    ;;
+    1)
+    #ejecucion
+    printf "\t \tNo \t \tNo \t \tSi\n"
+    ;;
+    2)
+    #escritura
+    printf "\t \tNo \t \tSi \t \tNo\n"
+    ;;
+    3)
+    #escritura y ejecucion
+    printf "\t \tNo \t \tSi \t \tSi\n"
+    ;;
+    4)
+    #lectura
+    printf "\t \tSi \t \tNo \t \tNo\n"
+    ;;
+    5)
+    #lectura y ejecucion
+    printf "\t \tSi \t \tNo \t \tSi\n"
+    ;;
+    6)
+    #lectura y escritura
+    printf "\t \tSi \t \tSi \t \tNo\n"
+    ;;
+    7)
+    #lectura, escritura y ejecucion
+    printf "\t \tSi \t \tSi \t \tSi\n"
+    ;;
+esac
+}
+
+error=0 #aqui pedimos lo datos, y comprobamos que el error es igual a 1, es decir, se cumplen las condiciones 
+while [ $error -eq 0 ]; 
+do 
     read -p "Introduce los permisos en octal: " permisos
     octal $permisos
     echo ""
 done
 
+numero1=$(echo $permisos | cut -c1)
+numero2=$(echo $permisos | cut -c2)
+numero3=$(echo $permisos | cut -c3)
 
+# printf "             Lectura        Escritura       Ejecución\n"
+# printf "Usuario"
+# opciones $numero1
+# printf "Grupo"
+# opciones $numero2
+# printf "Resto"
+# opciones $numero3
+# printf "\n"
 
-
+printf "             Lectura        Escritura       Ejecución\n"
+printf "Usuario"
+opciones $numero1
+printf "Grupo"
+opciones $numero2
+printf "Resto"
+opciones $numero3
+printf "\n"
