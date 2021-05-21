0ec5246a01191bbb374b43dca065f40d751631c0
alejandro-sierra <alejandrosierraballesta@gmail.com>
2021-05-21 12:58:31 +0200

    11.02 preparado

---
+++ ud11/11.02/ejercicio01/ejercicio01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#autor Alejandro Ballesta Sierras
+#ejercicio11.02-01
+# Crea un script que se le pase un nombre de archivo como parámetro y
+# muestre por pantalla que tipo de archivo es. Además, si es un directorio
+# mostrará su contenido y si es un fichero lo mostrará por pantalla.
